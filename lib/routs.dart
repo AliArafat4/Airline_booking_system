56643a4ebc517fdd279e0e60173b207997b087b2
HadiAlzayer <hadiessam9@gmail.com>
2022-05-01 23:53:35 +0300

    Home Page

--- lib/routs.dart
+++ lib/routs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:ics324_project/screens/forgot_password/forgot_password_screen.dart';
+import 'package:ics324_project/screens/home/home_screen.dart';
 import 'package:ics324_project/screens/login_success/login_success_screens.dart';
 import 'package:ics324_project/screens/sign_in/sign_in_screen.dart';
 import 'package:ics324_project/screens/splash/splash_screen.dart';
@@ -9,4 +10,5 @@ final Map<String, WidgetBuilder> routes = {
   signInScreen.routeName: (context) => signInScreen(),
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  HomeScreen.routName: (context) => HomeScreen(),
 };
---
+++ lib/screens/home/components/popular_products.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/Product.dart';
+import '../../../size_config.dart';
+import 'product_card.dart';
+import 'section_title.dart';
+
+class PopularProducts extends StatelessWidget {
+  const PopularProducts({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(
+          text: "Popular Product",
+          press: () {},
+        ),
+        SizedBox(
+          height: getPropertionatesScreenWidth(20),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ...List.generate(
+                demoProducts.length,
+                (index) => ProductCard(product: demoProducts[index]),
+              ),
+              SizedBox(
+                width: getPropertionatesScreenWidth(20),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
